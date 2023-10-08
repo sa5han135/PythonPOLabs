94258ea2f11a41b10f889497c4cdb85349d75362
sa5han135 <147332791+sa5han135@users.noreply.github.com>
2023-10-08 20:44:51 +0300

    Add files via upload

---
+++ Лабораторная работа 1/task_3.py
@@ -0,0 +1,10 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+
+len(list_players)  # Нахождение количества игроков, но без использования print()
+
+first_team = list_players[0:3]  # Создадим первую команду слайсированием
+print(first_team)
+second_team = list_players[3:6]  # Создадим вторую команду слайсированием
+print(second_team)
+
+
